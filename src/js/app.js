bbbe4305025dcbc411664749468a937a2d5d5c5f
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-02-18 17:18:27 +0100

    add dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   eventListeners();
+  darkMode();
 });
+
+const darkMode = () => {
+  const prefiereDarkMode = window.matchMedia("(prefers-color-schema:dark)");
+  //console.log(prefiereDarkMode.matches);
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+  prefiereDarkMode.addEventListener("change", function () {
+      if (prefiereDarkMode.matches) {
+        document.body.classList.add("dark-mode");
+      } else {
+        document.body.classList.remove("dark-mode");
+      }
+  })
+  const botonDarkMode = document.querySelector(".dark-mode-boton");
+  botonDarkMode.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
+};
 
 const eventListeners = () => {
   const mobileMenu = document.querySelector(".mobile-menu");
@@ -17,6 +39,6 @@ const eventListeners = () => {
     // }
 
     //OPCION 2
-    navegacion.classList.toggle("mostrar") //el toggle hace lo mismo que el código anterior pero en una linea
+    navegacion.classList.toggle("mostrar"); //el toggle hace lo mismo que el código anterior pero en una linea
   }
 };
